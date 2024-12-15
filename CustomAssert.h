f4cb5a058b958be1bf33e954a8888b97c37fd651
ashqqy <Artiom.shaff07@gmail.com>
2024-12-16 01:20:22 +0300

    exit & stderr

--- CustomAssert.h
+++ CustomAssert.h
@@ -5,26 +5,26 @@
 #include <stdlib.h>
 
 // soft assert
-#define CustomWarning(expression, return_message) do                                                      \
-{                                                                                                         \
-    if (!(expression))                                                                                    \
-    {                                                                                                     \
-        printf ("%s: %s:%d: Expression `%s' warning.\n", __FUNCTION__, __FILE__, __LINE__, #expression ); \
-        return return_message;                                                                            \
-    }                                                                                                     \
-}                                                                                                         \
-while (0)                                                                                                 \
+#define CustomWarning(expression, return_message) do                                                               \
+{                                                                                                                  \
+    if (!(expression))                                                                                             \
+    {                                                                                                              \
+        fprintf (stderr, "%s: %s:%d: Expression `%s' warning.\n", __FUNCTION__, __FILE__, __LINE__, #expression ); \
+        return return_message;                                                                                     \
+    }                                                                                                              \
+}                                                                                                                  \
+while (0)                                                                                                          \
 
 // hard assert
-#define CustomAssert(expression) do                                                                       \
-{                                                                                                         \
-    if (!(expression))                                                                                    \
-    {                                                                                                     \
-        printf ("%s: %s:%d: Assertion `%s' failed.\n", __FUNCTION__, __FILE__, __LINE__, #expression );   \
-        printf ("Program aborting\n");                                                                    \
-        abort();                                                                                          \
-    }                                                                                                     \
-}                                                                                                         \
-while (0)                                                                                                 \
+#define CustomAssert(expression) do                                                                              \
+{                                                                                                                \
+    if (!(expression))                                                                                           \
+    {                                                                                                            \
+        fprintf (stderr, "%s: %s:%d: Assertion `%s' failed.\n", __FUNCTION__, __FILE__, __LINE__, #expression )  \
+        fprintf (stderr, "Program aborting\n");                                                                  \
+        exit();                                                                                                  \
+    }                                                                                                            \
+}                                                                                                                \
+while (0)                                                                                                        \
 
 #endif // CUSTOM_ASSERT_H
